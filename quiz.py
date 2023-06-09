79c3c0e79d0c3b81fde3cd55c40be079a4ec6366
Liya-23 <116875322+Liya-23@users.noreply.github.com>
2023-06-09 16:05:44 +0200

    initial commit

---
+++ quiz.py
@@ -0,0 +1,25 @@
+print("The Hero Welcomes You ❤")
+wanna_play = input("You want to play?: ")
+
+if wanna_play != "yes":
+    quit()
+else:
+    print("ok")
+    
+
+answer = input("In which year was 'Not All Heroes Wear Capes' produced? ")
+if answer == "2018":
+    print("Correct!")
+else:
+    print("Incorrect!")
+    
+answer = input("In which year was 'Not All Heroes Wear Capes' produced? ")
+if answer == "2018":
+    print("Correct!")
+else:
+    print("Incorrect!")
+answer = input("In which year was 'Not All Heroes Wear Capes' produced? ")
+if answer == "2018":
+    print("Correct!")
+else:
+    print("Incorrect!")
