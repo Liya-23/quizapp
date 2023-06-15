f33c29212fa4035f9c56dea1d8273facd4d599af
El Heroe <116875322+Liya-23@users.noreply.github.com>
2023-06-15 15:43:05 +0200

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -13,13 +13,31 @@ if answer == "2018":
 else:
     print("Incorrect!")
     
-answer = input("In which year was 'Not All Heroes Wear Capes' produced? ")
-if answer == "2018":
+answer = input("In which year was 'Heroes and Villians' produced? ")
+if answer == "2022":
     print("Correct!")
 else:
     print("Incorrect!")
-answer = input("In which year was 'Not All Heroes Wear Capes' produced? ")
-if answer == "2018":
+answer = input("In which year was 'Spider-Man into the spider verse' produced? ")
+if answer == "2023":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("In which year was 'The Voice(deluxe)' produced? ")
+if answer == "2019":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("In which year was 'The Voice Of the Heroes' produced? ")
+if answer == "2020":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("In which year was 'Spider-Man into the spider verse' produced? ")
+if answer == "2023":
     print("Correct!")
 else:
     print("Incorrect!")
